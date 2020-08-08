4de2d2aacbe31af5e8911c76303655cf9cd596f9
AaronMullan <aaroncmullan@gmail.com>
2020-08-08 16:55:41 -0700

    added URL:: to css asset path

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('public/app.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('app.css') }}" rel="stylesheet">
   <title>@yield('title', 'default')</title>
 </head>
 <body>
